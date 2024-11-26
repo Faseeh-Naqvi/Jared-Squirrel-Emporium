133e031bf8cfba61efd30bd72cf0cc7420cefaad
faseeh-naqvi <faseehnaqvi100@gmail.com>
2024-11-26 11:16:24 -0500

    Enhance layout and functionality: added Bootstrap for responsive design, improved banner and navigation, and implemented dynamic welcome message based on the day of the week.

--- js/script.js
+++ js/script.js
@@ -8,10 +8,39 @@ window.addEventListener('scroll', () => {
 });
 
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const today = new Date().getDay();
-document.querySelector('#dynamic-message').textContent = `Welcome to our store on this ${days[today]}!`;
+function dayHello(){
+    var today = new Date().getDay();
 
+    switch(today){//Switch is easier than all those if statements. 
+        case 0://it is also easier to put the whole thing in one line instead of using an extra variable.
+            document.getElementById('dynamic-message').innerHTML = "🥜 Welcome to our store on this Sunday! 🥜";
+            break;
+        case 1:
+            document.getElementById('dynamic-message').innerHTML = "🥜 Welcome to our store on this Monday! 🥜";
+            break;
+        case 2:
+            document.getElementById('dynamic-message').innerHTML = "🥜 Welcome to our store on this Tuesday! 🥜";
+            break;
+        case 3:
+            document.getElementById('dynamic-message').innerHTML = "🥜 Welcome to our store on this Wednesday! 🥜";
+            break;
+        case 4:
+            document.getElementById('dynamic-message').innerHTML = "🥜 Welcome to our store on this Thursday! 🥜";
+            break;
+        case 5:
+            document.getElementById('dynamic-message').innerHTML = "🥜 Welcome to our store on this Friday! 🥜";
+            break;
+        case 6:
+            document.getElementById('dynamic-message').innerHTML = "🥜 Welcome to our store on this Saturday! 🥜";
+            break;
+        default:
+            document.getElementById('dynamic-message').innerHTML = "🥜 Welcome to our store! 🥜";
+        }
+}
+dayHello();
+
+var topper = document.getElementById('back-to-top');
 
 document.getElementById('back-to-top').addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
